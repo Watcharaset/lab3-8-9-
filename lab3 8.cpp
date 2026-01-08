f05fb94e38585838a061b43aa95f40fa22ab3c50
6804062611033 <s6804062611033@email.kmutnb.ac.th>
2026-01-08 16:26:22 +0700

    Add time calculation with user input for minutes

--- lab3 8.cpp
+++ lab3 8.cpp
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
 int main() {
-	int h,m,s;
+	int h,m,s,n;
 	scanf("%d:%d:%d",&h,&m,&s);
+	printf("hour:%d\nminute:%d\nsecond:%d\nNext minutes (0-120): ",h,m,s);
+	scanf("%d",&n);
+	m = m+n;
+	if (s>=60) {
+		m = m+1;
+		s = s-60;
+	}
+	if (m>=60 && m<120) {
+		h = h+1;
+		m = m-60;
+	}
+	else if (m>=120 && m<180) {
+		h = h+2;
+		m = m-120;
+	}
+	else {
+		printf("Error\n");
+	}
+	if (h>=24) {
+		h = 0;
+	}
 	printf("hour:%d\nminute:%d\nsecond:%d",h,m,s);
 }
